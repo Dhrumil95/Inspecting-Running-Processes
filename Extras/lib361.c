a31585d2e325d98800194c3e733b91203d20bd2a
Dhrumil Patel <DhrumilPa.95@Gmail.Com>
2016-05-23 18:38:58 -0500

    Initial Commit

---
+++ Extras/lib361.c
@@ -0,0 +1,15 @@
+
+
+int num = 0;
+
+int secretoperation() { 
+
+if ( num == 0 ) {
+num = 1;
+return 10;
+}
+else 
+return 4;
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Inspecting-Running-Processes
+interacting with precompiled programs as they execute.
